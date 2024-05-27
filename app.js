09255400f6e92083e49dc254679966d58d320c7e
kimminsanggg <rlaalstkd177@gmail.com>
2024-05-27 14:12:46 +0900

    step1

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.get("/home", (req, res)=>{
     res.end("<h1>home!</h1>");
 });
 
+app.get("/profile", (req, res)=>{
+    res.end("<h1>home!</h1>");
+});
+
+app.get("/shop", (req, res)=>{
+    res.end("<h1>shopping mall!</h1>");
+});
+
 const server = http.createServer(app);
 server.listen(3000, ()=>{
     console.log("Run on server 3000");
